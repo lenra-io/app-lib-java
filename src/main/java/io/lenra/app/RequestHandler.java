45f9ed2ea4804b6427520c9162c30e04f27ddd2e
Thomas DA ROCHA <thomas.darocha@lenra.io>
2024-03-10 17:39:34 +0100

    feat: Manage processor

--- src/main/java/io/lenra/app/RequestHandler.java
+++ src/main/java/io/lenra/app/RequestHandler.java
@@ -1,8 +1,34 @@
 package io.lenra.app;
 
+import io.lenra.api.AppRequest;
+import io.lenra.api.ListenerRequest;
+import io.lenra.api.Manifest;
+import io.lenra.api.ManifestRequest;
+import io.lenra.api.ResourceRequest;
 import io.lenra.api.ViewRequest;
+import io.lenra.api.ViewResponse;
 
 public abstract class RequestHandler {
-	public abstract Object handleView(ViewRequest request);
-	
+	public Object handle(AppRequest request) {
+		if (request instanceof ViewRequest) {
+			return handleView((ViewRequest) request);
+		} else if (request instanceof ListenerRequest) {
+			handleListener((ListenerRequest) request);
+			return null;
+		} else if (request instanceof ResourceRequest) {
+			return handleResource((ResourceRequest) request);
+		} else if (request instanceof ManifestRequest) {
+			return handleManifest((ManifestRequest) request);
+		} else {
+			throw new IllegalArgumentException("Unknown request type: " + request.getClass());
+		}
+	}
+
+	protected abstract ViewResponse handleView(ViewRequest request);
+
+	protected abstract void handleListener(ListenerRequest request);
+
+	protected abstract byte[] handleResource(ResourceRequest request);
+
+	protected abstract Manifest handleManifest(ManifestRequest request);
 }
---
+++ src/main/java/io/lenra/app/annotation/Listener.java
@@ -0,0 +1,20 @@
+package io.lenra.app.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Listener {
+	String name() default "";
+
+	@Target(ElementType.PARAMETER)
+	@Retention(RetentionPolicy.SOURCE)
+	public static @interface Props {}
+
+	@Target(ElementType.PARAMETER)
+	@Retention(RetentionPolicy.SOURCE)
+	public static @interface Api {}
+}
